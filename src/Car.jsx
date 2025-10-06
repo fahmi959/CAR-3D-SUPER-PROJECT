1780dbb650c1e67c5ab5820cb847233e95384515
Fahmi <fahmiardiansyah959@students.unnes.ac.id>
2025-10-06 19:02:33 +0700

    Update Car.jsx

--- src/Car.jsx
+++ src/Car.jsx
@@ -8,7 +8,7 @@ import { useWheels } from "./useWheels";
 import { WheelDebug } from "./WheelDebug";
 import AudioManager from "./AudioManager";
 
-export function Car({ thirdPerson }) {
+export function Car({ thirdPerson, touchControls }) {
   let result = useLoader(GLTFLoader, process.env.PUBLIC_URL + "/models/car.glb").scene;
 
   const position = [-1.5, 0.5, 3];
@@ -24,34 +24,28 @@ export function Car({ thirdPerson }) {
   );
 
   const [wheels, wheelInfos] = useWheels(width, height, front, wheelRadius);
-
   const [vehicle, vehicleApi] = useRaycastVehicle(
     () => ({ chassisBody, wheelInfos, wheels }),
     useRef(null)
   );
 
-  useControls(vehicleApi, chassisApi);
+  useControls(vehicleApi, chassisApi, touchControls);
 
-  // --- Velocity untuk AudioManager ---
   const velocity = useRef([0, 0, 0]);
-useEffect(() => {
-  const unsub = chassisApi.velocity.subscribe((v) => (velocity.current = v));
-  return unsub;
-}, []);
+  useEffect(() => {
+    const unsub = chassisApi.velocity.subscribe((v) => (velocity.current = v));
+    return unsub;
+  }, []);
 
-  // --- Camera third person ---
+  // Third person camera
   useFrame((state) => {
     if (!thirdPerson) return;
-
     const pos = new Vector3();
     pos.setFromMatrixPosition(chassisBody.current.matrixWorld);
-
     const quat = new Quaternion();
     quat.setFromRotationMatrix(chassisBody.current.matrixWorld);
-
     const wDir = new Vector3(0, 0, 1).applyQuaternion(quat).normalize();
     const camPos = pos.clone().add(wDir.clone().multiplyScalar(1).add(new Vector3(0, 0.3, 0)));
-
     state.camera.position.copy(camPos);
     state.camera.lookAt(pos);
   });
@@ -67,14 +61,8 @@ useEffect(() => {
       <group ref={chassisBody} name="chassisBody">
         <primitive object={result} rotation-y={Math.PI} position={[0, -0.09, 0]} />
       </group>
-
-      <WheelDebug wheelRef={wheels[0]} radius={wheelRadius} />
-      <WheelDebug wheelRef={wheels[1]} radius={wheelRadius} />
-      <WheelDebug wheelRef={wheels[2]} radius={wheelRadius} />
-      <WheelDebug wheelRef={wheels[3]} radius={wheelRadius} />
-
-      {/* --- AudioManager --- */}
-   <AudioManager velocityRef={velocity} />
+      {wheels.map((w, i) => <WheelDebug key={i} wheelRef={w} radius={wheelRadius} />)}
+      <AudioManager velocityRef={velocity} />
     </group>
   );
 }
